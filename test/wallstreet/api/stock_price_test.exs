7348e569eb6eab51088f3499dd4ed021af37bd60
EssenceOfChaos <fjschiller@gmail.com>
2018-11-27 23:07:02 -0500

    writing test for API

--- test/wallstreet/api/stock_price_test.exs
+++ test/wallstreet/api/stock_price_test.exs
@@ -1,7 +1,11 @@
 defmodule Wallstreet.StockPriceTest do
+  @moduledoc """
+  Defines the test suite for the WallStreet API
+  """
   use ExUnit.Case
 
-  test "returns a value" do
-    assert Wallstreet.Api.StockPrice.quote("aapl") !== nil
+  test "starts the GenServer" do
+   {:ok, pid} =  Wallstreet.Api.Server.start_link()
+   assert is_pid(pid) == true
   end
 end
